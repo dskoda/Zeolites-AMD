bf792ceda526eb00ac080d657d30622c0ec4342a
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2023-05-11 10:20:13 -0700

    update nbs and plotting

--- zeo_amd/plotting.py
+++ zeo_amd/plotting.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 FIGS_DIR = "../figs"
 
 
-def savefig(fig, name, prefix=FIGS_DIR):
+def savefig(fig, name, prefix=FIGS_DIR, **kwargs):
     dst = os.path.join(prefix, name)
-    fig.savefig(dst, bbox_inches="tight", transparent=True)
+    fig.savefig(dst, bbox_inches="tight", transparent=True, **kwargs)
     return dst
