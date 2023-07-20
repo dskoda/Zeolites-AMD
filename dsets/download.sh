cc9d3cc50ddc6203b71f132ca483685fa7f38e94
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2023-07-20 12:46:08 -0400

    download datasets

---
+++ dsets/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Downloads the optimized structures from Erlenbach et al (2022)
+# directly from their Zenodo repository
+# (https://doi.org/10.5281/zenodo.5827897)
+
+wget https://zenodo.org/record/5827897/files/DEEM_NNPscan.db
+wget https://zenodo.org/record/5827897/files/IZA_NNPscan.db
